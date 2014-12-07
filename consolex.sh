fe6bab00036ee69ac33b341a52df56d69f20f0e5
chr1573r <chr1573r@users.noreply.github.com>
2014-12-07 17:56:53 +0100

    Update consolex.sh

--- consolex.sh
+++ consolex.sh
@@ -3,6 +3,6 @@
 clear
 echo "ConsoleX"
 echo
-echo "Hello World!
-echo -n "Last update was: "; $(cat ../last_update.txt)
-sleep 300
+echo "Hello World!"
+echo -n "Last update was: $(cat ../last_update.txt)"
+sleep 60
