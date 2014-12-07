96a43958dde8a7a2da3e72049fcaf8ce8c89aee5
chr1573r <chr1573r@users.noreply.github.com>
2014-12-07 18:02:17 +0100

    Update consolex.sh

--- consolex.sh
+++ consolex.sh
@@ -5,4 +5,7 @@ echo "ConsoleX"
 echo
 echo "Hello World!"
 echo -n "Last update was: $(cat ../last_update.txt)"
+echo
+echo "This terminal supports $(tput lines) lines"
+echo "and $(tput cols) characters on each"
 sleep 60
