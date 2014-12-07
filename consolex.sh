45c7f3e12934c886e2b57d5e6946a047fc2885b5
chr1573r <chr1573r@users.noreply.github.com>
2014-12-07 21:34:20 +0100

    Update consolex.sh

--- consolex.sh
+++ consolex.sh
@@ -8,9 +8,9 @@ echo "Hello World!"
 echo "Last update was: $(cat ../last_update.txt)"
 echo
 echo "Current temperature: "
-toilet -f mono9 $(curl -s http://192.168.2.125/current.txt)
 echo
+figlet -t -c -f mono9 3.33$(curl -s http://192.168.2.125/current.txt)$(echo -e "\xb0")
 echo
 echo "This terminal supports $(tput lines) lines"
 echo "and $(tput cols) characters on each"
-sleep 60
+sleep 10
